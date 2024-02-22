40ae1ed30ce621b6f94495d70254ab8f4004e13f
Prashanth <prashanth.kuchhalapati@gmail.com>
2024-02-22 05:41:39 +0530

    home page created

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("this is homepage")
+
+
+def about(request):
+    return HttpResponse("this is aboutpage")
